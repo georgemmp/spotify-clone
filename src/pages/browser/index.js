910b8d0b9a17a7bbb0b784a0642ec174a856b484
georgemmp <machadogeorge.p@gmail.com>
2019-09-06 15:09:04 -0300

    Estilizando página de navegação de playlist

---
+++ src/pages/browser/index.js
@@ -0,0 +1,40 @@
+import React from  'react';
+
+import { Container, Title, List, Playlist } from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist>
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" alt="Player" />
+        <strong>Heavy, Heavy Metal!</strong>
+        <p>Porrada na orelha! Apenas Clássicos!</p>
+      </Playlist>
+      <Playlist>
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" alt="Player" />
+        <strong>Heavy, Heavy Metal!</strong>
+        <p>Porrada na orelha! Apenas Clássicos!</p>
+      </Playlist>
+      <Playlist>
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" alt="Player" />
+        <strong>Heavy, Heavy Metal!</strong>
+        <p>Porrada na orelha! Apenas Clássicos!</p>
+      </Playlist>
+      <Playlist>
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" alt="Player" />
+        <strong>Heavy, Heavy Metal!</strong>
+        <p>Porrada na orelha! Apenas Clássicos!</p>
+      </Playlist>
+      <Playlist>
+        <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg?auto=format&q=60&fit=max&w=930" alt="Player" />
+        <strong>Heavy, Heavy Metal!</strong>
+        <p>Porrada na orelha! Apenas Clássicos!</p>
+      </Playlist>
+    </List>
+
+  </Container>
+);
+
+export default Browse;
